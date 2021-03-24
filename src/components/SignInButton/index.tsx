9a7e4f1311fb800a5c8ab0d1f0f9a6f448105092
Dgeison <paradg@gmail.com>
2021-03-24 17:54:53 -0300

    SSR

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,19 @@
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import styles from "./styles.module.scss";
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Dgeison Peixoto
+      <FiX color="#73780" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
+      Sign in With Button
+    </button>
+  );
+}
